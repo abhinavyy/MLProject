3d8a78548b258157a8b708ee307a8568655755ba
Abhinav yadav <abhinavat3791@gmail.com>
2024-11-10 01:06:09 +0530

    pipeline and interface

--- src/utils.py
+++ src/utils.py
@@ -11,14 +11,14 @@ from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
 
 def save_object(file_path, obj):
-    """
-    Saves a Python object to the specified file path using dill.
-    """
     try:
         dir_path = os.path.dirname(file_path)
+
         os.makedirs(dir_path, exist_ok=True)
+
         with open(file_path, "wb") as file_obj:
-            dill.dump(obj, file_obj)
+            pickle.dump(obj, file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
     
@@ -52,4 +52,11 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
 
     except Exception as e:
         raise CustomException(e, sys)
- 
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
